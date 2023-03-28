d1d32a042818c2693b6333e9f7464828327f9b0f
kakum3 <104210758+kakum3@users.noreply.github.com>
2023-03-28 22:22:09 +0200

    ejercio15

---
+++ sesion15/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Tasklist from './components/lists/TaskList';
+
+function App() {
+  return (
+    <div className="App">
+      <Tasklist/>
+    </div>
+  );
+}
+
+export default App;
